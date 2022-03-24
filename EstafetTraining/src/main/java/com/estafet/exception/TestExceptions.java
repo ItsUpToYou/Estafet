6278d20441e6f55c6d3b61310b103a54c0ee7288
ItsUpToYou <v_kapitanov@yahoo.com>
2022-03-24 18:26:59 +0200

    Add new Estafet Traning

---
+++ EstafetTraining/src/main/java/com/estafet/exception/TestExceptions.java
@@ -0,0 +1,16 @@
+package com.estafet.exception;
+
+import org.junit.Test;
+
+public class TestExceptions {
+
+    public void main(String[] args) {
+        Order ord = new Order();
+        //ord.methodSignitureException(8);
+        //ord.methodHandleException(8);
+        //ord.methodReThrow(3);
+        //ord.methodCatchMultipleExceptions();
+        //ord.catchThrowableException();
+        ord.tryWithResources();
+    }
+}
