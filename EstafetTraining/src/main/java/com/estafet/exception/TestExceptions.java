25bcbca48ff23dd53cab9eff0c3eb8670666cb2b
ItsUpToYou <v_kapitanov@yahoo.com>
2022-03-28 19:08:02 +0300

    merge conflicts cleaning

--- EstafetTraining/src/main/java/com/estafet/exception/TestExceptions.java
+++ EstafetTraining/src/main/java/com/estafet/exception/TestExceptions.java
@@ -3,12 +3,9 @@ package com.estafet.exception;
 import org.junit.Test;
 
 public class TestExceptions {
-<<<<<<<< HEAD:EstafetTraining/src/com/estafet/exception/TestExceptions.java
-    public static void main(String[] args) {
-========
 
-    public void main(String[] args) {
->>>>>>>> origin/Maven:EstafetTraining/src/main/java/com/estafet/exception/TestExceptions.java
+    public static void main(String[] args) {
+
         Order ord = new Order();
         //ord.methodSignitureException(8);
         //ord.methodHandleException(8);
