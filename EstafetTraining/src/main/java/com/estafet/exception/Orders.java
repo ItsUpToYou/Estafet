e15bf943d8b9948482a919dd4cce25c845fc8c1a
ItsUpToYou <v_kapitanov@yahoo.com>
2022-03-30 17:03:37 +0300

    adding training project

---
+++ EstafetTraining/src/main/java/com/estafet/exception/Orders.java
@@ -0,0 +1,88 @@
+package com.estafet.exception;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class Orders implements Serializable {
+    private int orders_id;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String address;
+    private String email;
+    private int itemQuantity;
+    private int totalPrice;
+    private Date dateTime;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getItemQuantity() {
+        return itemQuantity;
+    }
+
+    public void setItemQuantity(int itemQuantity) {
+        this.itemQuantity = itemQuantity;
+    }
+
+    public int getTotalPrice() {
+        return totalPrice;
+    }
+
+    public void setTotalPrice(int totalPrice) {
+        this.totalPrice = totalPrice;
+    }
+
+    public Date getDateTime() {
+        return dateTime;
+    }
+
+    public void setDateTime(Date dateTime) {
+        this.dateTime = dateTime;
+    }
+
+    public int getOrders_id() {
+        return orders_id;
+    }
+
+    public void setOrders_id(int orders_id) {
+        this.orders_id = orders_id;
+    }
+}
